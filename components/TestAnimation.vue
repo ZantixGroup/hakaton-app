d7c01c0e99185383a179807c5cc7e5c0d73b3af8
miksme <miks_mednis@inbox.lv>
2023-09-29 17:37:55 +0300

    feat: UWAAH

---
+++ components/TestAnimation.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div style="width: 400px;">
+      <div style="width: 20px; height: 20px; background-color: blue;" class="el"></div>
+      <input ref="obj" class="progress" step="0.01" type="range" min="0" max="100" value="0" />
+    </div>
+  </div>
+</template>
+<script>
+import anime from 'animejs';
+
+export default {
+  name: 'TestAnimation',
+  mounted(){
+    this.$nextTick(()=>{
+      setTimeout(() => {
+        var animation = anime({
+        targets: '.el',
+        translateX: 270,
+        delay: function(el, i) { return i * 100; },
+        elasticity: 200,
+        easing: 'easeInOutSine',
+        autoplay: false
+      });
+      // this.$refs.obj.innerHTML
+
+      var seekProgressEl = document.querySelector('.progress');
+      seekProgressEl.oninput = function() {
+        animation.seek(animation.duration * (seekProgressEl.value / 100));
+      };
+      }, 300);
+      
+    })
+  }
+}
+</script>
