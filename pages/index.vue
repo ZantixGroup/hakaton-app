7fd045744712037886f61705e2a0b7324dcdf240
Alberts Liepins <liepins01@gmail.com>
2023-09-30 23:58:00 +0300

    feat: prog

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center" class="bg">
     <v-col cols="12">
       <v-card flat>
-        <div :class="($vuetify.breakpoint.mobile? $vuetify.breakpoint.xs?'mobile-grid':'mobile-grid':'d-flex justify-center margin-5')">
+        <div :class="($vuetify.breakpoint.mobile? $vuetify.breakpoint.xs?'mobile-flex':'mobile-flex':'desktop-grid')">
           <SubjectBlock @click="changeToCategory('science')" title="Zinātne" description="Sistemātiska un kārtota patiesības meklēšana" color="red" score="25" maxScore="100"/>
           <SubjectBlock @click="changeToCategory('mathematics')" title="Matemātika" description="Abstrakts skaitļu un lielumu pētījums" color="green" score="25" maxScore="100"/>
           <SubjectBlock @click="changeToCategory('engineering')" title="Indžinierija" description="Izstrādā risinājumus reāliem izaicinājumiem" color="blue" score="25" maxScore="100"/>
@@ -28,10 +28,9 @@ export default {
     }
   }
 }
-
 </script>
 <style scoped lang="scss">
-.mobile-grid {
+.mobile-flex {
   display: flex;
   flex-wrap: wrap;
   gap: 5px;
@@ -40,13 +39,21 @@ export default {
     flex: 1 0 calc(50% - 5px / 2);
     margin: 0px !important;
   }
+
+  & > * {
+    margin: 5px;
+  }
 }
 
-.mobile-grid > * {
-  margin: 5px;
-}
-.margin-5 > * {
-    margin: 5px;
+.desktop-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 5px;
+
+  & > :global(.v-card){
+    margin: 0px !important;
+  }
 }
 
 .col {
