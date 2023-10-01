3635d8f93b47b5ba3eaef13f35c2e6ac5b83d802
SodiumN1trate <renarsgausins21@gmail.com>
2023-10-01 03:48:39 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -3,36 +3,36 @@
     <v-col cols="12">
       <v-card flat>
         <div :class="($vuetify.breakpoint.mobile? $vuetify.breakpoint.xs?'mobile-flex':'mobile-flex':'desktop-grid')">
-          <SubjectBlock 
-            @click="changeToCategory('science')" 
-            title="Zinātne" 
-            description="Sistemātiska un kārtota patiesības meklēšana" 
+          <SubjectBlock
+            @click="changeToCategory('science')"
+            title="Zinātne"
+            description="Sistemātiska un kārtota patiesības meklēšana"
             color="red"
-            score="25"
+            :score="s_level"
             maxScore="100"
             svgPath="/svg/Science.svg"/>
-          <SubjectBlock 
-            @click="changeToCategory('mathematics')" 
-            title="Matemātika" 
-            description="Abstrakts skaitļu un lielumu pētījums" 
-            color="green" 
-            score="25" 
+          <SubjectBlock
+            @click="changeToCategory('mathematics')"
+            title="Matemātika"
+            description="Abstrakts skaitļu un lielumu pētījums"
+            color="green"
+            :score="m_level"
             maxScore="100"
             svgPath="/svg/Math.svg"/>
-          <SubjectBlock 
-            @click="changeToCategory('engineering')" 
-            title="Indžinierija" 
-            description="Izstrādā risinājumus reāliem izaicinājumiem" 
-            color="blue" 
-            score="25" 
+          <SubjectBlock
+            @click="changeToCategory('engineering')"
+            title="Indžinierija"
+            description="Izstrādā risinājumus reāliem izaicinājumiem"
+            color="blue"
+            :score="e_level"
             maxScore="100"
             svgPath="/svg/Engineering.svg"/>
           <SubjectBlock
-            @click="changeToCategory('technology')" 
-            title="Tehnoloģija" 
-            description="Inovatīvi rīki kas veido mūsu nākotni" 
-            color="orange" 
-            score="25" 
+            @click="changeToCategory('technology')"
+            title="Tehnoloģija"
+            description="Inovatīvi rīki kas veido mūsu nākotni"
+            color="orange"
+            :score="t_level"
             maxScore="100"
             svgPath="/svg/Technology.svg"/>
         </div>
@@ -45,11 +45,26 @@
 <script>
 import SubjectBlock from '~/components/SubjectBlock.vue';
 import TestAnimation from '~/components/TestAnimation.vue';
+import { UserData } from '../storage/user.ts'
 
 export default {
   name: 'IndexPage',
   auth: false,
   components: { TestAnimation, SubjectBlock },
+  computed: {
+    s_level () {
+      return UserData.S_level
+    },
+    t_level () {
+      return UserData.T_level
+    },
+    e_level () {
+      return UserData.E_level
+    },
+    m_level () {
+      return UserData.M_level
+    }
+  },
   methods: {
     changeToCategory(e){
       this.$router.push({ name: e})
@@ -77,7 +92,7 @@ export default {
   margin-top: 4rem;
   display: grid;
   grid-template-columns: auto auto auto auto;
-  gap: 5px;
+  gap: 10px;
 
   & > :global(.v-card){
     margin: 0px !important;
